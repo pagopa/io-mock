08d333af426fc78ee10ffee5f64090f5306c2c14
Daniele Manni <dan.manni@gmail.com>
2020-09-29 14:57:31 +0200

    [#174950220] Update and rebuild on remote changes

---
+++ io-bonus-inps-node-mock/scripts/sync-and-start.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Update the remote references
+git fetch
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+# Rebuild the application after an codebase update
+rebuild() {
+    yarn install
+    yarn build
+}
+
+# Start the application
+start() {
+    yarn start
+}
+
+if [ $LOCAL = $REMOTE ]; then
+    # The local branch is Up to date
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    # Remote branch has newest commits
+    git pull
+    rebuild
+elif [ $REMOTE = $BASE ]; then
+    # Some commits was removed from remote branch
+    git reset --hard @{u}
+    rebuild
+else
+    # Local branch and remote branch are diverged
+    git reset --hard @{u}
+    rebuild
+fi
+
+start
