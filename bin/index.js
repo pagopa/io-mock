9f40b7c53133892c95b0dfcebad40277431c145e
Daniele Manni <dan.manni@gmail.com>
2020-10-08 12:36:38 +0200

    [#175141691] COMMIT_SHA update at buildtime

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,28 @@
 const dotenv = require("dotenv");
 const envsub = require("envsub");
 const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
 dotenv.config();
 
-const [_, __, repoUrlEnv, repoBranchEnv, templatePath, destinationPath] = process.argv;
+const [_, __, templatePath, destinationPath] = process.argv;
 (async () => {
+  const repositories = JSON.parse(fs.readFileSync(path.join(__dirname, "../config/repositories.json")).toString());
   const headers = { "Authorization": `token ${process.env["GITHUB_TOKEN"]}`};
-  const commitSha = await fetch(`${process.env[repoUrlEnv]}${process.env[repoBranchEnv]}`, {headers})
-    .then(res => res.json())
-    .then(json => json.object.sha);
- 
+  const envs = await Promise.all(repositories.map(async _ => 
+    {
+      return {
+        name: _.ENV_COMMIT_SHA,
+        value: await fetch(`${_.GITHUB_REPO}${_.BRANCH}`, {headers})
+        .then(res => res.json())
+        .then(json => json.object.sha)
+      }
+      
+    }));
     let templateFile = `${__dirname}/../${templatePath}`;
     let outputFile = `${__dirname}/../${destinationPath}`;
     let options = {
-      envs: [
-        {name: "COMMIT_SHA", value: commitSha}
-      ],
+      envs,
       envFiles: [
         `${__dirname}/../.env`
       ],
