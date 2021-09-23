fe2958b34300f2df485b5f5779c6ded77bbf88a4
Michael Disaro <michael.disaro@gmail.com>
2021-09-23 18:26:32 +0200

    removed useless files

--- scripts/generate-spid-certs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t italia-backend/tools:1.0.0 docker/images/tools
-docker run --rm -v "$PWD:/usr/src/app" -e "NODE_ENV=development" -w "/usr/src/app" italia-backend/tools:1.0.0 yarn build
-docker run --rm -v "$PWD/../backend:/usr/src/app" -e "NODE_ENV=development" -w "/usr/src/app/certs" italia-backend/tools:1.0.0 yarn generate:test-certs
