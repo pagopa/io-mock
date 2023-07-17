73fb5fffc35e7a600f0b297b76d4ac39fd37d644
gquadrati <greta.quadrati@pagopa.it>
2023-07-17 19:00:01 +0200

    more verbose error

--- bin/index.js
+++ bin/index.js
@@ -7,27 +7,38 @@ dotenv.config();
 
 const [_, __, templatePath, destinationPath] = process.argv;
 (async () => {
-  const repositories = JSON.parse(fs.readFileSync(path.join(__dirname, "../config/repositories.json")).toString());
-  const headers = { "Authorization": `token ${process.env["GITHUB_TOKEN"]}`};
-  const envs = await Promise.all(repositories.map(async _ => 
-    {
+  const repositories = JSON.parse(
+    fs
+      .readFileSync(path.join(__dirname, "../config/repositories.json"))
+      .toString()
+  );
+  const headers = { Authorization: `token ${process.env["GITHUB_TOKEN"]}` };
+  const envs = await Promise.all(
+    repositories.map(async (_) => {
       return {
         name: _.ENV_COMMIT_SHA,
-        value: await fetch(`${_.GITHUB_REPO}${_.BRANCH}`, {headers})
-        .then(res => res.json())
-        .then(json => json.object.sha)
-      }
-      
-    }));
-    let templateFile = `${__dirname}/../${templatePath}`;
-    let outputFile = `${__dirname}/../${destinationPath}`;
-    let options = {
-      envs,
-      envFiles: [
-        `${__dirname}/../.env`
-      ],
-      syntax: 'default'
-    };
-    
-    await envsub({templateFile, outputFile, options});
-})().catch(err => console.error(err));
+        value: await fetch(`${_.GITHUB_REPO}${_.BRANCH}`, { headers })
+          .then((res) => res.json())
+          .then((json) => {
+            let sha;
+            try {
+              sha = json.object.sha;
+            } catch (error) {
+              console.warn("Sha is empty for ", _.GITHUB_REPO);
+            }
+
+            return sha;
+          }),
+      };
+    })
+  );
+  let templateFile = `${__dirname}/../${templatePath}`;
+  let outputFile = `${__dirname}/../${destinationPath}`;
+  let options = {
+    envs,
+    envFiles: [`${__dirname}/../.env`],
+    syntax: "default",
+  };
+
+  await envsub({ templateFile, outputFile, options });
+})().catch((err) => console.error(err));
